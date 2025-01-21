9a8127626da8628ae6e3a67a174943e09e15aae8
Eduardo Kobus <eduardo.kobus@senior.com.br>
2025-01-20 22:15:13 -0300

    Add  absolute imports

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-    res.status(200).json({chave: "Nothing here!"})
+import database from "infra/database.js"
+
+async function status(req, res) {
+    const result =  await database.query("SELECT 1+1 as sum");
+    console.log(result);
+    res.status(200).json({status: "ok"});
 }
 
 export default status;
