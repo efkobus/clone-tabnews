9bceb4e93b1c10272a468e3b5978ba8ea30bcec9
Eduardo Kobus <eduardo.kobus@senior.com.br>
2025-01-21 22:40:34 -0300

    build status API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js"
 
 async function status(req, res) {
-    const result =  await database.query("SELECT 1+1 as sum");
-    console.log(result);
-    res.status(200).json({status: "ok"});
+    const updatedAt = new Date().toISOString();
+
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+    const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
+    const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
+
+    const databaseOpenConnectionsResult = await database.query(
+        "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
+    );
+
+    const databaseOpenConnectionsValue = 
+        databaseOpenConnectionsResult.rows[0].count;
+
+    res.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersionValue,
+                max_connections: parseInt(databaseMaxConnectionValue),
+                open_connections: databaseOpenConnectionsValue,
+            },
+        },
+    });
 }
 
 export default status;
