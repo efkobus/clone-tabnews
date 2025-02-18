358cc774fd583d4f9b4672482b2db1f4d5a7ee4c
Eduardo Kobus <eduardo.kobus@senior.com.br>
2025-02-18 08:15:43 -0300

    adds jest config

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,18 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase() {
+   await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+   const res = await fetch("http://localhost:3000/api/v1/migrations",{
+         method: "POST",
+   });
+   expect(res.status).toBe(200);
+
+   const responseBody = await res.json();
+
+   expect(Array.isArray(responseBody)).toBe(true);
+});
