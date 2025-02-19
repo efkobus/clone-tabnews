8defba2200e33cbce48f4f0832df7fc58852065c
Eduardo Kobus <eduardo.kobus@senior.com.br>
2025-02-18 21:55:48 -0300

    change migration options

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,29 +2,32 @@ import migrationRunner from 'node-pg-migrate'
 import { join } from "node:path";
 
 export default async function migrations(req, res) {
+    
+    const defaultMigrationOptions = {
+        databaseUrl: process.env.DATABASE_URL,
+        dryRun: true,
+        dir: join("infra", "migrations"),
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations",
+    }
 
     if (req.method === "GET") {
-        const migrations = await migrationRunner({
-            databaseUrl: process.env.DATABASE_URL,
-            dryRun: true,
-            dir: join("infra", "migrations"),
-            direction: "up",
-            verbose: true,
-            migrationsTable: "pgmigrations",
-        });
+        const migrations = await migrationRunner(defaultMigrationOptions);
         return res.status(200).json(migrations);
     }
 
     if (req.method === "POST") {
-        const migrations = await migrationRunner({
-            databaseUrl: process.env.DATABASE_URL,
-            dryRun: false,
-            dir: join("infra", "migrations"),
-            direction: "up",
-            verbose: true,
-            migrationsTable: "pgmigrations",
+        const migratedMigrations = await migrationRunner({
+            ...defaultMigrationOptions,
+            dryRun: false
         });
-        return res.status(200).json(migrations);
+
+        if (migratedMigrations.length > 0) {
+            return res.status(201).json(migratedMigrations);
+        }
+
+        return res.status(200).json(migratedMigrations);
     }
 
     return res.status(405).end();
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,13 +6,25 @@ async function cleanDatabase() {
    await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
 }
 
-test("POST to /api/v1/migrations should return 200", async () => {
+test("POST to /api/v1/migrations should return 201", async () => {
    const res = await fetch("http://localhost:3000/api/v1/migrations",{
          method: "POST",
    });
-   expect(res.status).toBe(200);
+   expect(res.status).toBe(201);
 
    const responseBody = await res.json();
 
    expect(Array.isArray(responseBody)).toBe(true);
+   expect(responseBody.length).toBeGreaterThan(0);
+});
+
+test("POST to /api/v1/migrations should return 200", async () => {
+   const res2 = await fetch("http://localhost:3000/api/v1/migrations",{
+         method: "POST",
+   });
+   expect(res2.status).toBe(200);
+
+   const response2Body = await res2.json();
+
+   expect(Array.isArray(response2Body)).toBe(true);
 });
